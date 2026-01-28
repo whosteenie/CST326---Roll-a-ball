5c30b1b560aa74c6a2af526e23455262743688ac
Justin <justinmnmj@gmail.com>
2026-01-27 21:14:03 -0800

    changed to SF and updated gitignore

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,20 @@ using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour {
-    private Rigidbody _rb;
-    private MeshRenderer _renderer;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private MeshRenderer meshRenderer;
 
     private Vector2 _movementVector;
     private float _movementX;
     private float _movementY;
-
     public float speed;
 
     [SerializeField] private UIDocument uiDocument;
     private Label _labelWin;
     private Label _labelCount;
-    private int _count;
 
     [SerializeField] private GameObject pickUpParent;
+    private int _count;
     private int _maxCount;
 
     [SerializeField] private GameObject enemy;
@@ -27,12 +26,10 @@ public class PlayerController : MonoBehaviour {
     private bool _gameOver;
 
     private void Start() {
-        _rb = GetComponent<Rigidbody>();
-        _renderer = GetComponent<MeshRenderer>();
-
         var root = uiDocument.rootVisualElement;
         _labelCount = root.Q<Label>("labelCount");
         _labelWin = root.Q<Label>("labelWin");
+        
         _maxCount = pickUpParent.transform.childCount;
         _count = 0;
 
@@ -40,10 +37,10 @@ public class PlayerController : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        if(_gameOver && _renderer.enabled == false) return;
+        if(_gameOver && meshRenderer.enabled == false) return;
 
         var movement = new Vector3(_movementX, 0.0f, _movementY);
-        _rb.AddForce(movement * speed);
+        rb.AddForce(movement * speed);
 
         CheckOutOfBounds();
     }
@@ -59,9 +56,9 @@ public class PlayerController : MonoBehaviour {
     private void OnJump(InputValue value) {
         if(!_gameOver) return;
 
-        _rb.isKinematic = false;
+        rb.isKinematic = false;
         ResetPlayerPos();
-        _renderer.enabled = true;
+        meshRenderer.enabled = true;
 
         _labelWin.style.visibility = Visibility.Hidden;
         _count = 0;
@@ -85,19 +82,19 @@ public class PlayerController : MonoBehaviour {
     }
 
     private void SetCountText() {
-        _labelCount.text = " Count: " + _count;
+        _labelCount.text = $" Count: {_count}";
     }
 
     private void ResetPlayerPos() {
-        _rb.linearVelocity = Vector3.zero;
-        _rb.angularVelocity = Vector3.zero;
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
         transform.position = new Vector3(0f, 0.5f, 0f);
     }
 
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("Enemy")) {
-            _renderer.enabled = false;
-            _rb.isKinematic = true;
+            meshRenderer.enabled = false;
+            rb.isKinematic = true;
             _labelWin.text = "You Lose!";
             _labelWin.style.visibility = Visibility.Visible;
             _gameOver = true;
